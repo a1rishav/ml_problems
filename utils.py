76d338039a484dec5500a9d376b1ad7ed5b8649a
urishav <mail.rishav.kr@gmail.com>
2020-08-09 22:45:14 +0530

    removed unused file

--- utils.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def save_to_file(file_name, obj):
-    with open(file_name, 'wb') as f:
-        pickle.dump(obj, f)
-
-def save_to_file(file_name, obj):
-    with open(file_name, 'wb') as f:
-        pickle.dump(obj, f)
